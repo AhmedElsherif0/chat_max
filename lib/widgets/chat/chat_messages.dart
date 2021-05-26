61ffa79f082bca183729dc986667073eb2963c87
ahmed <ahmed.elsherif010@gmail.com>
2021-05-26 18:48:12 +0200

    after solving

--- lib/widgets/chat/chat_messages.dart
+++ lib/widgets/chat/chat_messages.dart
@@ -4,6 +4,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
+  final Stream<QuerySnapshot> _messageStream = FirebaseFirestore.instance
+      .collection('chat')
+      .orderBy('createdAt', descending: true)
+      .snapshots(includeMetadataChanges: true);
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -12,24 +17,24 @@ class ChatMessages extends StatelessWidget {
           if (futureSnapshot.connectionState == ConnectionState.waiting)
             return Center(child: CircularProgressIndicator());
           return StreamBuilder(
-              stream: FirebaseFirestore.instance.collection('chat')
-                  .orderBy('createdAt', descending: true)
-                  .snapshots(),
-              builder: (context, streamSnapShots) {
-                if (streamSnapShots.connectionState == ConnectionState.waiting) {
+              stream: _messageStream,
+              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapShots) {
+                if (streamSnapShots.connectionState ==
+                    ConnectionState.waiting) {
                   return Center(child: CircularProgressIndicator());
                 }
-                final data = streamSnapShots.data.docs;
+                final data = streamSnapShots.data!.docs;
                 return ListView.builder(
                   reverse: true,
                   itemCount: data.length,
                   itemBuilder: (ctx, index) => Container(
                     padding: EdgeInsets.all(8),
                     child: MessageBubble(
-                      data.docs[index]['text'],
-                      data.docs[index]['username'],
-                      data.docs[index]['userImage'],
-                      data.docs[index]['userId'] == _currentUser().uid,
+                      data[index]['text'],
+                      data[index]['username'],
+                      data[index]['userImage'],
+                      data[index]['userId'] ==
+                          FirebaseAuth.instance.currentUser?.uid,
                     ),
                   ),
                 );
@@ -37,7 +42,5 @@ class ChatMessages extends StatelessWidget {
         });
   }
 
-  _currentUser() async {
-    FirebaseAuth.instance.currentUser;
-  }
+  _currentUser() async {}
 }
